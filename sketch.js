5475ea1e9bb9542001b530841a62f6ebf44bd562
elo <00001126385554sp@al.educacao.sp.gov.br>
2024-06-25 19:40:02 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria(){
+  let palavras = ["SUGA", "or AGUST D", " or YOONGI"];
+  return random(palavras);
+}
+
+function inicializaCores() {
+
+  background("rgb(51,44,44)");
+  fill("rgb(230,220,220)");
+  textSize(44);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw() {
+
+  inicializaCores();
+
+  let texto = palavraParcial(0, width);
+  text(texto, 200, 200);
+}
